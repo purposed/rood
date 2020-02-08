a9003372103e1af0b31498369466eb4b3a4d338c
Dalloriam <dalloriam@gmail.com>
2020-02-08 16:16:47 -0500

    Start work on stacked output manager

--- src/cli/output.rs
+++ src/cli/output.rs
@@ -20,43 +20,59 @@ fn make_padding(length: i32) -> String {
 }
 
 pub struct OutputManager {
-    pub verbose: bool
+    pub verbose: bool,
+    padding: i32
 }
 
 impl OutputManager {
     pub fn new(verbose: bool) -> OutputManager {
         OutputManager{
-            verbose
+            verbose,
+            padding: 0
         }
     }
 
-    fn print(&self, msg: ColoredString, prefix_marker: &str, padding: i32, verbose_only: bool) {
+    pub fn push(&self) -> OutputManager {
+        OutputManager{
+            verbose: self.verbose,
+            padding: self.padding+1
+        }
+    }
+
+    pub fn with_padding(&self, padding: i32) -> OutputManager {
+        OutputManager{
+            verbose: self.verbose,
+            padding: self.padding+1
+        }
+    }
+
+    fn print(&self, msg: ColoredString, prefix_marker: &str, verbose_only: bool) {
         if !self.verbose && verbose_only {
             return;
         }
 
-        let pad = make_padding(padding);
+        let pad = make_padding(self.padding);
         println!("{}{} {}", pad, prefix_marker, msg);
     }
 
-    pub fn step(&self, msg: &str, padding: i32) {
-        self.print(msg.yellow(), STEP_PREFIX_MARKER, padding, false);
+    pub fn step(&self, msg: &str) {
+        self.print(msg.yellow(), STEP_PREFIX_MARKER, false);
     }
 
-    pub fn progress(&self, msg: &str, padding: i32) {
-        self.print(ColoredString::from(msg), NO_PREFIX, padding, false);
+    pub fn progress(&self, msg: &str) {
+        self.print(ColoredString::from(msg), NO_PREFIX, false);
     }
 
-    pub fn success(&self, msg: &str, padding: i32) {
-        self.print(msg.green(), STEP_PREFIX_MARKER, padding, false);
+    pub fn success(&self, msg: &str) {
+        self.print(msg.green(), STEP_PREFIX_MARKER, false);
     }
 
-    pub fn debug(&self, msg: &str, padding: i32) {
-        self.print(msg.blue(), DEBUG_PREFIX_MARKER, padding, true);
+    pub fn debug(&self, msg: &str) {
+        self.print(msg.blue(), DEBUG_PREFIX_MARKER, true);
     }
 
-    pub fn error(&self, msg: &str, padding: i32) {
-        self.print(msg.red(), ERROR_PREFIX_MARKER, padding, false);
+    pub fn error(&self, msg: &str) {
+        self.print(msg.red(), ERROR_PREFIX_MARKER, false);
     }
 
 }
